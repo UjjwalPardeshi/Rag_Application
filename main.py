0a310b8320fb42ff964317aecfc282f6c61a55a1
Ujjwal Pardeshi <ujjwalpardeshi@gmail.com>
2024-06-11 23:03:52 +0530

    added inital dependencies

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello')
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+population_path = os.path.join("data", "Population.csv") 
+population_df = pd.read_csv(population_path)
+
+print(population_df.head())
+
+
+
+load_dotenv
